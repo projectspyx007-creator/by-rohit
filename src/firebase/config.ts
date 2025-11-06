9661c1b7ade6ea46d1173597827e01f21b19c017
Sachin Kumar <cs24b1050@iiitr.ac.in>
2025-11-06 16:40:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1233603019-d3eb1",
+  "appId": "1:638727943046:web:5e526658992ca406c37207",
+  "apiKey": "AIzaSyDeh5QZSpTvrYbdirrQS9XtWOd3MiN-IVM",
+  "authDomain": "studio-1233603019-d3eb1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "638727943046"
+};
